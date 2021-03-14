c8cf6e6e59891a88450c0a27971d1564d0a9b786
orsteve@yandex.ru <orsteve@yandex.ru>
2021-03-15 02:36:08 +0300

    fix

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,30 @@
-
-describe('My Login application', () => {
+describe('JSON.org page', () => {
     before(() => {
         browser.enablePerformanceAudits()
     })
-    it('should login with valid credentials', () => {
-        browser.url("https://scores24.live/ru");
+
+    it('should load within performance budget', () => {
+        /**
+         * this page load will take a bit longer as the DevTools service will
+         * capture all metrics in the background
+         */
+        browser.url('http://json.org')
+
         let metrics = browser.getMetrics()
-        console.log(metrics)
-    });
+        assert.ok(metrics.speedIndex < 1500) // check that speedIndex is below 1.5ms
+
+        let score = browser.getPerformanceScore() // get Lighthouse Performance score
+        assert.ok(score >= .99) // Lighthouse Performance score is at 99% or higher
+
+        $('=Esperanto').click()
+
+        metrics = browser.getMetrics()
+        assert.ok(metrics.speedIndex < 1500)
+        score = browser.getPerformanceScore()
+        assert.ok(score >= .99)
+    })
+
     after(() => {
         browser.disablePerformanceAudits()
     })
-});
+})
--- wdio.conf.js
+++ wdio.conf.js
@@ -53,6 +53,7 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
+        acceptInsecureCerts: true,
         'goog:chromeOptions': {
             args: ['incognito', '--headless']
         }
@@ -109,7 +110,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver', 'devtools'],
+    services: ['devtools', 'chromedriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
