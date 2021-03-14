82a5d395d63e9075c890ca2de7a5be7f98eb6b94
orsteve@yandex.ru <orsteve@yandex.ru>
2021-03-15 02:06:38 +0300

    fix

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -3,6 +3,6 @@ describe('My Login application', () => {
     it('should login with valid credentials', () => {
         browser.url("https://scores24.live/ru");
         browser.pause(2000);
-        expect(browser).toHaveTitle('Результаты матчей и прогнозы на спорт каждый день - Scores24')
+        expect(browser).toHaveTitle('Результаты матчей и прогнозы на спорт каждый день - Scores24.live')
     });
 });
